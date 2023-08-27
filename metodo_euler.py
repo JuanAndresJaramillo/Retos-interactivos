1bba89667747d44e1f6f848877058950dae5280b
Juan Andrés Jaramillo <juan_jaramillo91201@elpoli.edu.co>
2023-08-27 18:33:04 -0500

    Evaluamos funcion

--- metodo_euler.py
+++ metodo_euler.py
@@ -4,13 +4,15 @@ def metodo_euler( EDO, X0, Xf, Y0, h ):
     y = Y0
     i = 0
     print( "--"*6 )
-    print( f"{i} \t {X0} \t {y}")
+    print( f"{i} \t \t {X0} \t {y}")
     for x in range(X0, Xf, h):
         resultado = eval(EDO)
         y += h * resultado
         i += 1
         print( "--"*6 )
         print( f"{i} \t {x} \t {y}")
+
+resul = metodo_euler("2*x+y", 0, 1, 2, 0.2)
 
         
     
