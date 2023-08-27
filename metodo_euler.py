c3ee653a55f92c3f30c437b0a82ce9268b73e712
Juan Andrés Jaramillo <juan_jaramillo91201@elpoli.edu.co>
2023-08-27 18:25:18 -0500

    Terminamos la funcion

--- metodo_euler.py
+++ metodo_euler.py
@@ -1,8 +1,16 @@
 
 def metodo_euler( EDO, X0, Xf, Y0, h ):
     print( f"Iteracion \t X \t Y")
-    for i in range(X0, Xf, h):
-        EDO = EDO.replace("X", i)
+    y = Y0
+    i = 0
+    print( "--"*6 )
+    print( f"{i} \t {X0} \t {y}")
+    for x in range(X0, Xf, h):
+        resultado = eval(EDO)
+        y += h * resultado
+        i += 1
+        print( "--"*6 )
+        print( f"{i} \t {x} \t {y}")
 
         
     
