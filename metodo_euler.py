939e067195148902030f5dbf451686988430a2f7
Juan Andrés Jaramillo <juan_jaramillo91201@elpoli.edu.co>
2023-08-27 18:44:36 -0500

    mejoramos funcion

--- metodo_euler.py
+++ metodo_euler.py
@@ -1,13 +1,18 @@
 
-def metodo_euler( EDO, X0, Xf, Y0, h ):
+def metodo_euler( EDO, X0, N, Y0, h ):
+    """
+    Esta funcion resuelve EDOs de primer grado
+    por medio del metodo numerico de euler
+    """
     print( f"Iteracion \t X \t Y")
-    y = Y0
-    i = 0
+    y = Y0  
+    x = X0
     print( "--"*6 )
-    print( f"{i} \t \t {X0} \t {y}")
-    for x in range(X0, Xf, h):
+    print( f"{0} \t \t {X0} \t {y}")
+    for i in range( 1, N ):
         resultado = eval(EDO)
         y += h * resultado
+        x += h
         i += 1
         print( "--"*6 )
         print( f"{i} \t {x} \t {y}")
