683e2ced0cefcfd48eabdcdc25a1721ee4ead89a
Juan Andrés Jaramillo <juan_jaramillo91201@elpoli.edu.co>
2023-08-27 20:12:52 -0500

    Funciones de ingresar numeros

--- metodo_euler.py
+++ metodo_euler.py
@@ -18,6 +18,26 @@ def metodo_euler( EDO, X0, Y0, N, h ):
     print("\n")
 
 
+def ingresar_numero_entero_positivo(mensaje):
+    bandera = 0
+    while (bandera == 0):
+        numero = int(input(mensaje))
+        if numero > 0:
+            bandera = 1
+        else:
+            print( "Digite un numero entero positivo\n")
+    return numero
 
+def ingresar_numero_real_positivo(mensaje):
+    bandera = 0
+    while (bandera == 0):
+        numero = float(input(mensaje))
+        if numero > 0:
+            bandera = 1
+        else:
+            print( "Digite un numero real positivo\n")
+    return numero
+    
+    
         
     
