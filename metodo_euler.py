791e72768105ee583d16e9c3a760d5a1e74b1b97
Juan Andrés Jaramillo <juan_jaramillo91201@elpoli.edu.co>
2023-08-29 21:41:30 -0500

    Mejoramos las funciones de ingresar numeros

--- metodo_euler.py
+++ metodo_euler.py
@@ -21,9 +21,13 @@ def metodo_euler( EDO, X0, Y0, N, h ):
 def ingresar_numero_entero_positivo(mensaje):
     bandera = 0
     while (bandera == 0):
-        numero = int(input(mensaje))
-        if numero > 0:
-            bandera = 1
+        numero = input(mensaje)
+        if numero.isdigit() == True:
+            numero = int(numero)
+            if numero > 0:
+                bandera = 1
+            else:
+               print( "Digite un numero entero positivo\n") 
         else:
             print( "Digite un numero entero positivo\n")
     return numero
@@ -31,9 +35,13 @@ def ingresar_numero_entero_positivo(mensaje):
 def ingresar_numero_real_positivo(mensaje):
     bandera = 0
     while (bandera == 0):
-        numero = float(input(mensaje))
-        if numero > 0:
-            bandera = 1
+        numero = input(mensaje)
+        if numero.isalnum() == False and numero.isalpha() == False:
+            numero = float(numero)
+            if numero > 0:
+                bandera = 1
+            else:
+                print( "Digite un numero real positivo\n")
         else:
             print( "Digite un numero real positivo\n")
     return numero
